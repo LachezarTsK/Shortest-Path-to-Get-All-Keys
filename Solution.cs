3084ab3bd26d9001e3fb7258bb4c3481fac4f3d7
Lachezar Kutsarov <lkutsarov@gmail.com>
2023-09-17 14:01:49 +0200

    Create Solution.cs

---
+++ Solution.cs
@@ -0,0 +1,171 @@
+
+using System;
+using System.Collections.Generic;
+
+public class Solution
+{
+    private class Step
+    {
+        public int row;
+        public int column;
+        public int bitStamp;
+        public Step(int row, int column, int bitStamp)
+        {
+            this.row = row;
+            this.column = column;
+            this.bitStamp = bitStamp;
+        }
+    }
+
+    private static readonly char WALL = '#';
+    private static readonly char START = '@';
+    private static readonly char EMPTY_POINT = '.';
+    private static readonly int PATH_NOT_FOUND = -1;
+    private static readonly int[] POINT_NOT_FOUND = { -1, -1 };
+    private static readonly int[][] MOVES = { new int[]{ -1, 0 },
+                                              new int[]{ 1, 0 },
+                                              new int[]{ 0, -1 },
+                                              new int[]{ 0, 1 }
+                                            };
+
+    private int rows;
+    private int columns;
+    private char[,] matrix;
+    public int ShortestPathAllKeys(string[] matrix)
+    {
+        rows = matrix.Length;
+        columns = matrix[0].Length;
+        FillMatrix(matrix);
+
+        int[] startPoint = FindStartPoint();
+        int bitSampAllKeys = CalculateBitSampAllKeys();
+
+        return SearchForShortestPathToGetAllKeys(startPoint, bitSampAllKeys);
+    }
+
+
+    private int SearchForShortestPathToGetAllKeys(int[] startPoint, int bitSampAllKeys)
+    {
+        Queue<Step> queue = new Queue<Step>();
+        queue.Enqueue(new Step(startPoint[0], startPoint[1], 0));
+
+        bool[,,] visitedByBitStamp = new bool[rows, columns, bitSampAllKeys + 1];
+        visitedByBitStamp[startPoint[0], startPoint[1], 0] = true;
+
+        matrix[startPoint[0], startPoint[1]] = EMPTY_POINT;
+        int shortestPathToGetAllKeys = 0;
+
+        while (queue.Count != 0)
+        {
+            int stepsInCurrentLevel = queue.Count;
+
+            while (stepsInCurrentLevel-- > 0)
+            {
+                Step currentStep = queue.Dequeue();
+                if (currentStep.bitStamp == bitSampAllKeys)
+                {
+                    return shortestPathToGetAllKeys;
+                }
+
+                foreach (int[] move in MOVES)
+                {
+                    int nextRow = currentStep.row + move[0];
+                    int nextColumn = currentStep.column + move[1];
+
+                    if (!IsInMatrix(nextRow, nextColumn) || visitedByBitStamp[nextRow, nextColumn, currentStep.bitStamp])
+                    {
+                        continue;
+                    }
+
+                    visitedByBitStamp[nextRow, nextColumn, currentStep.bitStamp] = true;
+
+                    if (matrix[nextRow, nextColumn] == EMPTY_POINT
+                            || (IsLock(matrix[nextRow, nextColumn]) && HasKey(matrix[nextRow, nextColumn], currentStep.bitStamp)))
+                    {
+                        queue.Enqueue(new Step(nextRow, nextColumn, currentStep.bitStamp));
+                        continue;
+                    }
+
+                    if (IsKey(matrix[nextRow, nextColumn]))
+                    {
+                        int newBitStamp = currentStep.bitStamp | CalculateBitSamp(matrix[nextRow, nextColumn]);
+                        queue.Enqueue(new Step(nextRow, nextColumn, newBitStamp));
+                    }
+                }
+            }
+            ++shortestPathToGetAllKeys;
+        }
+        return PATH_NOT_FOUND;
+    }
+
+    private void FillMatrix(String[] matrix)
+    {
+        this.matrix = new char[rows, columns];
+        for (int r = 0; r < rows; ++r)
+        {
+            for (int c = 0; c < columns; ++c)
+            {
+                this.matrix[r, c] = matrix[r][c];
+            }
+        }
+    }
+
+    private int[] FindStartPoint()
+    {
+        int[] startPoint = new int[2];
+        for (int r = 0; r < rows; ++r)
+        {
+            for (int c = 0; c < columns; ++c)
+            {
+                if (matrix[r, c] == START)
+                {
+                    startPoint[0] = r;
+                    startPoint[1] = c;
+                    return startPoint;
+                }
+            }
+        }
+        return POINT_NOT_FOUND;
+    }
+
+    private int CalculateBitSampAllKeys()
+    {
+        int bitSampAllKeys = 0;
+        for (int r = 0; r < rows; ++r)
+        {
+            for (int c = 0; c < columns; ++c)
+            {
+                if (IsKey(matrix[r, c]))
+                {
+                    bitSampAllKeys = bitSampAllKeys | CalculateBitSamp(matrix[r, c]);
+                }
+            }
+        }
+        return bitSampAllKeys;
+    }
+
+    private int CalculateBitSamp(char letter)
+    {
+        return 1 << (letter - 'a');
+    }
+
+    private bool IsInMatrix(int row, int column)
+    {
+        return row >= 0 && row < rows && column >= 0 && column < columns;
+    }
+
+    private bool IsKey(char letter)
+    {
+        return letter >= 'a' && letter <= 'f';
+    }
+
+    private bool IsLock(char letter)
+    {
+        return letter >= 'A' && letter <= 'F';
+    }
+
+    private bool HasKey(char letter, int currentBitStamp)
+    {
+        return (currentBitStamp & CalculateBitSamp(Char.ToLower(letter))) != 0;
+    }
+}
