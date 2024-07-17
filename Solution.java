d2be5c17d80ecb1333fb5a20d02ac2cb79f3257a
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-07-17 12:49:34 +0200

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -25,16 +25,16 @@ public class Solution {
         fillMatrix(matrix);
 
         int[] startPoint = findStartPoint();
-        int bitSampAllKeys = calculateBitSampAllKeys();
+        int bitStampAllKeys = calculateBitStampAllKeys();
 
-        return searchForShortestPathToGetAllKeys(startPoint, bitSampAllKeys);
+        return searchForShortestPathToGetAllKeys(startPoint, bitStampAllKeys);
     }
 
-    private int searchForShortestPathToGetAllKeys(int[] startPoint, int bitSampAllKeys) {
+    private int searchForShortestPathToGetAllKeys(int[] startPoint, int bitStampAllKeys) {
         Queue<Step> queue = new LinkedList<>();
         queue.add(new Step(startPoint[0], startPoint[1], 0));
 
-        boolean[][][] visitedByBitStamp = new boolean[rows][columns][bitSampAllKeys + 1];
+        boolean[][][] visitedByBitStamp = new boolean[rows][columns][bitStampAllKeys + 1];
         visitedByBitStamp[startPoint[0]][startPoint[1]][0] = true;
 
         matrix[startPoint[0]][startPoint[1]] = EMPTY_POINT;
@@ -45,7 +45,7 @@ public class Solution {
 
             while (stepsInCurrentLevel-- > 0) {
                 Step currentStep = queue.poll();
-                if (currentStep.bitStamp == bitSampAllKeys) {
+                if (currentStep.bitStamp == bitStampAllKeys) {
                     return shortestPathToGetAllKeys;
                 }
 
@@ -98,19 +98,19 @@ public class Solution {
         return POINT_NOT_FOUND;
     }
 
-    private int calculateBitSampAllKeys() {
-        int bitSampAllKeys = 0;
+    private int calculateBitStampAllKeys() {
+        int bitStampAllKeys = 0;
         for (int r = 0; r < rows; ++r) {
             for (int c = 0; c < columns; ++c) {
                 if (isKey(matrix[r][c])) {
-                    bitSampAllKeys = bitSampAllKeys | calculateBitSamp(matrix[r][c]);
+                    bitStampAllKeys = bitStampAllKeys | calculateBitStamp(matrix[r][c]);
                 }
             }
         }
-        return bitSampAllKeys;
+        return bitStampAllKeys;
     }
 
-    private int calculateBitSamp(char letter) {
+    private int calculateBitStamp(char letter) {
         return 1 << (letter - 'a');
     }
 
